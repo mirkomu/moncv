1ed68adda396b79ebc4082ab2674cad9c0d8cfc7
normandparatte <normand.paratte@gmail.com>
2017-06-29 09:21:56 +0200

    - Ajout d'un effet fadeIn sur les icones - Ajout de commentaires sur le main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,13 @@ import './main.css';
 import Chart from 'chart.js';
 
 $(document).ready(function () {
+    // ----- Effet sur les icones -----
+    $('.img-contact, .icones').hide().fadeIn(1500, 'linear');
+
+    // ----- Remplacement des progressbar par des chart -----
+    // boucle sur les progressbar
     $('.progress').each(function (index) {
+        // Préparation des données en reprenant les valeurs des progressbar
         let data = {
             datasets: [{
                 data: [$('.progress-bar').attr('aria-valuenow'), 100 - $('.progress-bar').attr('aria-valuenow')],
@@ -22,6 +28,7 @@ $(document).ready(function () {
             }]
         };
 
+        // Création du graphique
         let graphiqueHTML = $(this);
         graphiqueHTML.replaceWith('<canvas></canvas>');
         /* eslint-disable no-new */
@@ -31,6 +38,7 @@ $(document).ready(function () {
             options: Chart.defaults.doughnut
         });
 
+        // Remplace la progressbar actuelle par le chart créé en mémoire
         $(this).replaceWith(graphiqueHTML);
     });
 });
